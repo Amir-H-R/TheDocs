8fbb7d486f902e5d2cdb4948ceec846dd5e916fd
Amir Hossein <amir.h.rouh@gmail.com>
2023-11-11 14:51:10 +0330

    AppointmentEntity

---
+++ Persistance/Entities/Appointment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Entities
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public virtual Staff Staff { get; set; }
+        public int StaffId { get; set; }
+        public virtual Patient Patient { get; set; }
+        public int PatientId { get; set; }
+    }
+}
