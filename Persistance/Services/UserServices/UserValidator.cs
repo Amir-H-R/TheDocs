baf8c791bd0df0a4191c2b77574fae87c55339fa
Amir Hossein <amir.h.rouh@gmail.com>
2023-11-25 14:43:24 +0330

    UsernamePasswordEnglishOnlyRegex

--- Persistance/Services/UserServices/UserValidator.cs
+++ Persistance/Services/UserServices/UserValidator.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Persistance.Services.UserServices
@@ -13,8 +14,14 @@ namespace Persistance.Services.UserServices
     {
         public static ResultDto Validate(UserDto dto, IModel1 context)
         {
-            var username = context.People.Where(p => p.UserName == dto.Username).FirstOrDefault();
-            var idCardNumber = context.People.Where(p => p.IdCardNumber == dto.IdCardNumebr).FirstOrDefault();
+            Person username = context.People.Where(p => p.UserName == dto.Username).FirstOrDefault();
+            Person idCardNumber = context.People.Where(p => p.IdCardNumber == dto.IdCardNumebr).FirstOrDefault();
+
+            string engChar = "^[A-Za-z\\d_-]+$";
+            bool passwordMatch = Regex.IsMatch(dto.Password, engChar);
+            bool usernameMatch = Regex.IsMatch(dto.Username, engChar);
+            if (!passwordMatch || !usernameMatch)
+                return new ResultDto() { Message = "لطفا در فیلد های پسورد و نام کاربری فقط از حروف انگلیسی، _ ، - و اعداد استفاده کنید", Success = false };
             if (dto.Username == null || dto.IdCardNumebr == null || dto.PhoneNumber == null || dto.LastName == null || dto.Name == null || dto.Age == null || dto.Password == null)
                 return new ResultDto { Message = "لطفا همه مقادیر را وارد کنید", Success = false };
             if (String.IsNullOrWhiteSpace(dto.Username) || String.IsNullOrWhiteSpace(dto.IdCardNumebr) || String.IsNullOrWhiteSpace(dto.PhoneNumber) || String.IsNullOrWhiteSpace(dto.LastName) || String.IsNullOrWhiteSpace(dto.Name) || dto.Age == null || dto.Age == 0 || String.IsNullOrWhiteSpace(dto.Password))
